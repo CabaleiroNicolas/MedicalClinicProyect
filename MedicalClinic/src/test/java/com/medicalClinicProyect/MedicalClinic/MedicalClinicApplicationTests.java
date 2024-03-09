e3f96562404c807e620536f69b83cb32eaf4ee04
Cabaleiro Nicolas <92488038+CabaleiroNicolas@users.noreply.github.com>
2024-03-09 19:56:43 -0300

    Creación de proyecto

---
+++ MedicalClinic/src/test/java/com/medicalClinicProyect/MedicalClinic/MedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalClinicProyect.MedicalClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
